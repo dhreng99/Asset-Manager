c788063cef373a4721f986a15bd42e7a3b73b61c
dhreng99 <dominic.eng@me.com>
2024-11-04 12:37:34 +0000

    user validation

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=150)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    
+    def validate_username(self, username):
+        from app import User
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class AssetForm(FlaskForm):
+    name = StringField('Asset Name', validators=[DataRequired(), Length(max=200)])
+    description = TextAreaField('Description', validators=[Length(max=500)])
